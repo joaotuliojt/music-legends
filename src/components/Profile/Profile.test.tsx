1f2f8acc9c92362fba23224ae612dc5269f54062
João Túlio <joaotuliosoares@hotmail.com>
2022-08-05 13:10:12 -0300

    feat: add testing libray (#6)

--- src/components/Profile/Profile.test.tsx
+++ src/components/Profile/Profile.test.tsx
@@ -1,13 +1,24 @@
-import { create } from 'react-test-renderer'
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom'
+import { ThemeProvider } from '@emotion/react'
+import { render, screen } from '@testing-library/react'
 import { Profile } from '.'
-
+import Theme from '../../chackra/theme'
 describe('Profile component test', () => {
   it('component show user name', () => {
     const name = 'Jhon Doe'
-    const profileRender = create(<Profile name={name} userImageUrl="" />)
+    render(
+      <ThemeProvider theme={Theme}>
+        <Profile name={name} />
+      </ThemeProvider>
+    )
+
+    expect(screen.getByText(name)).toHaveTextContent(name)
+    /* const profileRender = create(<Profile name={name} />)
     const profileInstace = profileRender.root
 
     expect(profileInstace.props.name).toBe(name)
-    expect(profileRender.toJSON()).toMatchSnapshot()
+    expect(profileRender.toJSON()).toMatchSnapshot() */
   })
 })
